d6d4097e06cdcceb8dbe1f4c2728519f93bfc92c
Ricardo Peres <ricardo.neto027@gmail.com>
2026-07-27 15:27:41 +0100

    Add files via upload

---
+++ sprint-7-automation-setup/main.py
@@ -0,0 +1,46 @@
+import data
+import helpers
+
+
+class TestUrbanRoutes:
+
+    @classmethod
+    def setup_class(cls):
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print("Conectado ao servidor Urban Routes")
+        else:
+            print("Não foi possível conectar ao Urban Routes. "
+                  "Verifique se o servidor está ligado e ainda em execução.")
+
+    def test_set_route(self):
+        # Adicionar em S8
+        pass
+
+    def test_select_plan(self):
+        # Adicionar em S8
+        pass
+
+    def test_fill_phone_number(self):
+        # Adicionar em S8
+        pass
+
+    def test_fill_card(self):
+        # Adicionar em S8
+        pass
+
+    def test_comment_for_driver(self):
+        # Adicionar em S8
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        # Adicionar em S8
+        pass
+
+    def test_order_2_ice_creams(self):
+        for i in range(2):
+            # Adicionar em S8
+            pass
+
+    def test_car_search_model_appears(self):
+        # Adicionar em S8
+        pass
